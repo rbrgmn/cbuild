0d2befd652def37dd6a0db253963788d775f7edc
Roman Bergman <roman.bergman@tutanota.com>
2026-02-09 21:03:21 +0100

    add counter

--- main.c
+++ main.c
@@ -3,5 +3,11 @@
 int main() {
   printf("HELLO WORLD!!!\n");
   printf("I love write code in C!!!!\n");
+
+  int i = 0;
+  for (; i < 100; i++) {
+  }
+  printf("I counter: %d\n", i);
+  
   return 0;
 }
